04d5d30226bb3f68f3b84b47d574bc89cb202382
Itskillerluc <lvgraef@zeelandnet.nl>
2023-05-14 22:21:58 +0200

    fixed error

--- src/main/java/io/github/nano/devilry/blockentity/MortarEntity.java
+++ src/main/java/io/github/nano/devilry/blockentity/MortarEntity.java
@@ -2,43 +2,23 @@ package io.github.nano.devilry.blockentity;
 
 import io.github.nano.devilry.data.recipes.ModRecipeTypes;
 import io.github.nano.devilry.data.recipes.MortarRecipe;
-import io.github.nano.devilry.events.ModSoundEvents;
-import io.github.nano.devilry.item.ModItems;
 import io.github.nano.devilry.util.tags.DevilryTags;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.Container;
-import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.*;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.Optional;
 //todo
 
 public class MortarEntity extends BaseContainerBlockEntity
@@ -145,6 +125,10 @@ public class MortarEntity extends BaseContainerBlockEntity
 
     @Override
     public void clearContent() {
+
+    }
+
+    public void tick(){
 
     }
 }
